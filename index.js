71b06b0326d3cc979c57c3883ceeb30f78207dcb
David John <david.john@mail.utoronto.ca>
2021-05-09 11:18:08 -0400

    Added Procfile

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // We require the following so we can make use of them below.
 const express = require('express');
 const fetch = require('node-fetch');
+const ISO6391 = require('iso-639-1');
+
 
 // Initialize our app
 let app = express();
@@ -10,6 +12,7 @@ app.set("view engine", "ejs");  // Making ejs the view engine so we can render e
 app.use(express.static('public'));  // Allowing our app to use the files in public which include styles.css and styles.js files to be used.
 
 app.get("/", function (req, res) {
+  console.log(ISO6391.getCode("ENglish"));
   res.render("index");
 });
 
@@ -19,12 +22,65 @@ app.get("/all", function (req, res) {
   .then(data => renderAllCountries(data, res));  // The function renderWebpage is run with the retrieved JSON data.
 });
 
-function renderAllCountries(completeData, response) {
-  console.log(completeData);
-  res.render("index");
+app.get("/by-name", function (req, res) {
+  res.redirect("/");
+});
 
-  //response.render("index", {hatchways: hatchwaysData, candidateAvg: averages}); // Our index.ejs file within our views folder is passed the hatchwaysData passed into this function and uses the response passed in as well to render our web application.
+app.get("/by-full-name", function (req, res) {
+  res.redirect("/");
+});
+
+app.get("/by-country-code", function (req, res) {
+  res.redirect("/");
+});
+
+app.get("/by-currency", function (req, res) {
+  res.redirect("/");
+});
+
+app.get("/by-language", function (req, res) {
+  res.redirect("/");
+});
+
+app.get("/by-capital-city", function (req, res) {
+  res.redirect("/");
+});
+
+app.get("/by-calling-code", function (req, res) {
+  res.redirect("/");
+});
+
+app.get("/by-region", function (req, res) {
+  res.redirect("/");
+});
+
+app.get("/by-regional-block", function (req, res) {
+  res.redirect("/");
+});
+
+
+app.post("/all", function (req, res) {
+  fetch('https://restcountries.eu/rest/v2/all')
+  .then(res => res.json()) // the .json() method parses the JSON response into a JS object literal
+  .then(data => renderAllCountries(data, res));  // The function renderWebpage is run with the retrieved JSON data.
+});
+
+app.post("/by-name", function (req, res) {
+  console.log(req.body.);
+
+  fetch('https://restcountries.eu/rest/v2/name/united')
+  .then(res => res.json()) // the .json() method parses the JSON response into a JS object literal
+  .then(data => renderCountriesByName(data, res));  // The function renderWebpage is run with the retrieved JSON data.
+});
+
+function renderAllCountries(completeData, response) {
+  response.render("allCountries", {countriesData: completeData});
 }
+
+function renderCountriesByName(completeData, response) {
+  response.render("countriesByName", {countriesData: completeData});
+}
+
 
 
 
